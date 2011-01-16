855f7579ccda13e96e6e8567429b48df093e97c2
Damian Nurzynski <dnurzynski@gmail.com>
2011-01-16 12:29:24 +0100

    Fix bug: Message expectation counts don't work in combination with a stub - Issue #28

--- spec/rspec/mocks/multiple_return_value_spec.rb
+++ spec/rspec/mocks/multiple_return_value_spec.rb
@@ -35,6 +35,15 @@ module RSpec
           %Q|(Mock "mock").message(any args)\n    expected: 3 times\n    received: 4 times|
         )
       end
+
+      it "doesn't complain when there are too many calls but method is stubbed too" do
+        @mock.stub(:message).and_return :stub_result
+        @mock.message.should == @return_values[0]
+        @mock.message.should == @return_values[1]
+        @mock.message.should == @return_values[2]
+        @mock.message.should == :stub_result
+        expect { @mock.rspec_verify }.to_not raise_error(RSpec::Mocks::MockExpectationError)
+      end
     end
 
     describe "a Mock expectation with multiple return values with a specified count equal to the number of values" do
@@ -72,6 +81,19 @@ module RSpec
           %Q|(Mock "mock").message(any args)\n    expected: 3 times\n    received: 4 times|
         )
       end
+
+      it "complains when there are too many calls and method is stubbed too" do
+        third = Object.new
+        @mock.stub(:message).and_return :stub_result
+        @mock.message.should == @return_values[0]
+        @mock.message.should == @return_values[1]
+        @mock.message.should == @return_values[2]
+        @mock.message.should == :stub_result
+        expect { @mock.rspec_verify }.to raise_error(
+          RSpec::Mocks::MockExpectationError, 
+          %Q|(Mock "mock").message(any args)\n    expected: 3 times\n    received: 4 times|
+        )
+      end
     end
 
     describe "a Mock expectation with multiple return values specifying at_least less than the number of values" do
@@ -94,6 +116,26 @@ module RSpec
           %Q|(Mock "mock").message(no args)\n    expected: 2 times\n    received: 1 time|
         )
       end
+
+      context "when method is stubbed too" do
+        before { @mock.stub(:message).and_return :stub_result }
+
+        it "uses the stub return value for subsequent calls" do
+          @mock.message.should equal(11)
+          @mock.message.should equal(22)
+          @mock.message.should equal(:stub_result)
+          @mock.rspec_verify
+        end
+
+        it "fails when called less than the specified number" do
+          @mock.message.should equal(11)
+          expect { @mock.rspec_verify }.to raise_error(
+            RSpec::Mocks::MockExpectationError, 
+            %Q|(Mock "mock").message(no args)\n    expected: 2 times\n    received: 1 time|
+          )
+        end
+      end
+
     end
 
     describe "a Mock expectation with multiple return values with a specified count larger than the number of values" do
@@ -126,6 +168,22 @@ module RSpec
           RSpec::Mocks::MockExpectationError, 
           %Q|(Mock "mock").message(any args)\n    expected: 3 times\n    received: 4 times|
         )
+      end
+
+      context "when method is stubbed too" do
+        before { @mock.stub(:message).and_return :stub_result }
+
+        it "fails when called greater than the specified number" do
+          @mock.message.should equal(11)
+          @mock.message.should equal(22)
+          @mock.message.should equal(22)
+          @mock.message.should equal(:stub_result)
+          expect { @mock.rspec_verify }.to raise_error(
+            RSpec::Mocks::MockExpectationError, 
+            %Q|(Mock "mock").message(any args)\n    expected: 3 times\n    received: 4 times|
+          )
+        end
+
       end
     end
   end
