4bf0d5be9417cc989fb632020835513f7eb90b6a
Sidu Ponnappa <ckponnappa@gmail.com>
2011-03-15 05:41:39 +0530

    All any_instance specs created thus far are now green

---
+++ lib/rspec/mocks/any_instance.rb
@@ -0,0 +1,128 @@
+require 'ruby-debug'
+module RSpec
+  module Mocks
+    module AnyInstance
+      class StubChain
+        InvocationOrder = {
+          :with => [:stub],
+          :and_return => [:with, :stub]
+        }
+        
+        def initialize(method_name, *args, &block)
+          @messages = []
+          record(:stub, [method_name] + args, block)
+        end
+        
+        def with(*args, &block)
+          record(:with, args, block)
+        end
+
+        def and_return(*args, &block)
+          record(:and_return, args, block)
+        end
+
+        def record(rspec_method_name, args, block)
+          verify_invocation_order(rspec_method_name, args, block)
+          @messages << [args.unshift(rspec_method_name), block]
+          self
+        end
+        
+        def verify_invocation_order(rspec_method_name, args, block)
+          if rspec_method_name != :stub && !InvocationOrder[rspec_method_name].include?(last_message)
+            raise(NoMethodError, "Undefined method #{method_name}")
+          end
+        end
+        
+        def last_message
+          @messages.last.first.first
+        end
+        
+        def playback!(target)
+          @messages.inject(target) do |target, message|
+            target.__send__(*message.first, &message.last)
+          end
+        end
+      end
+      
+      class Recorder
+        def initialize(klass)
+          @stubs = {}
+          @klass = klass
+        end
+
+        def stub(method_name, *args, &block)
+          observe!(method_name)
+          @stubs[method_name.to_sym] = StubChain.new(method_name, *args, &block)
+        end
+        
+        def playback!(target, method_name)
+          @stubs[method_name].playback!(target)
+        end
+        
+        def observed_methods
+          @stubs.keys
+        end
+        
+        def stop_observing_currently_observed_methods!
+          observed_methods.each do |method_name|
+            stop_observing!(method_name)
+          end
+        end
+
+        def stop_observing!(method_name)
+          alias_method_name = "__#{method_name}_without_any_instance__"
+          if @klass.respond_to?(alias_method_name)
+            restore_original_method!(alias_method_name)
+          else
+            remove_dummy_method!(method_name)
+          end
+        end
+
+        def restore_original_method!(alias_method_name)
+          @klass.class_eval do
+            alias_method  method_name, alias_method_name
+            remove_method alias_method_name
+          end
+        end
+        
+        def remove_dummy_method!(method_name)
+          @klass.class_eval do
+            remove_method method_name
+          end
+        end
+        
+        def observe!(method_name)
+          @klass.class_eval do
+            if respond_to?(method_name)
+              alias_method "__#{method_name}_without_any_instance__", method_name
+            end
+            define_method(method_name) do |*args, &blk|
+              self.class.__recorder.playback!(self, method_name)
+              self.send(method_name, *args, &blk)
+            end
+          end
+        end
+      end
+
+      def any_instance
+        RSpec::Mocks::space.add(self) if RSpec::Mocks::space
+        __recorder
+      end
+
+      def rspec_reset
+        __recorder.stop_observing_currently_observed_methods!
+        @__recorder = nil
+        response = super
+        response
+      end
+
+      def reset?
+        !@__recorder && super
+      end
+
+      def __recorder
+        @__recorder ||= AnyInstance::Recorder.new(self)
+      end
+    end
+  end
+end
--- spec/rspec/mocks/any_instance_spec.rb
+++ spec/rspec/mocks/any_instance_spec.rb
@@ -3,30 +3,35 @@ require 'spec_helper'
 module RSpec
   module Mocks
     describe "#any_instance" do
-      let(:klass) { Class.new }
+      let(:klass) do 
+        klass = Class.new
+        klass.class_eval{ def ooga;2;end }
+        klass
+      end
       
       it "should raise an error if the method chain is in the wrong order" do
-        pending
         lambda{ klass.any_instance.with("1").stub(:foo) }.should raise_error(NoMethodError)
       end
       
       context "with #stub" do
         it "should not suppress an exception when a method that doesn't exist is invoked" do
-          pending
           klass.any_instance.stub(:foo)
           lambda{ klass.new.bar }.should raise_error(NoMethodError)
         end
       end
       
       context "with #and_return" do
-        it "stubs a method on any instance of a particular class" do
-          pending
+        it "stubs a method that doesn't exist on any instance of a particular class" do
           klass.any_instance.stub(:foo).and_return(1)
           klass.new.foo.should == 1
         end
 
+        it "stubs a method that exists on any instance of a particular class" do
+          klass.any_instance.stub(:ooga).and_return(1)
+          klass.new.ooga.should == 1
+        end
+
         it "returns the same object for calls on different instances" do
-          pending
           return_value = Object.new
           klass.any_instance.stub(:foo).and_return(return_value)
           klass.new.foo.should be(return_value)
@@ -36,13 +41,11 @@ module RSpec
       
       context "with a block" do
         it "stubs a method on any instance of a particular class" do
-          pending
           klass.any_instance.stub(:foo) { 1 }
           klass.new.foo.should == 1
         end
 
         it "returns the same computed value for calls on different instances" do
-          pending
           klass.any_instance.stub(:foo) { 1 + 2 }
           klass.new.foo.should == klass.new.foo
         end
@@ -50,7 +53,6 @@ module RSpec
       
       context "when resetting after an example" do
         it "restores the class to its original state after each example" do
-          pending
           space = RSpec::Mocks::Space.new
           space.add(klass)
           klass.any_instance.stub(:foo).and_return(1)
@@ -60,52 +62,44 @@ module RSpec
         end
       
         it "adds a class to the current space when #any_instance is invoked" do
-          pending
           klass.any_instance
           RSpec::Mocks::space.send(:mocks).should include(klass)
         end
       end
       
-      context "ensuring core ruby objects aren't clobbered" do
+      context "core ruby objects" do
         it "should work uniformly across *everything*" do
-          pending
           Object.any_instance.stub(:foo).and_return(1)
           Object.new.foo.should == 1
         end
         
         it "should work with the non-standard constructor []" do
-          pending
           Array.any_instance.stub(:foo).and_return(1)
           [].foo.should == 1
         end
         
         it "should work with the non-standard constructor {}" do
-          pending
           Hash.any_instance.stub(:foo).and_return(1)
           {}.foo.should == 1
         end
         
         it "should work with the non-standard constructor \"\"" do
-          pending
           String.any_instance.stub(:foo).and_return(1)
           "".foo.should == 1
         end
         
         it "should work with the non-standard constructor \'\'" do
-          pending
           String.any_instance.stub(:foo).and_return(1)
           ''.foo.should == 1
         end
 
         it "should work with the non-standard constructor module" do
-          pending
           Module.any_instance.stub(:foo).and_return(1)
           module RSpec::SampleRspecTestModule;end
           RSpec::SampleRspecTestModule.foo.should == 1
         end
         
         it "should work with the non-standard constructor class" do
-          pending
           Class.any_instance.stub(:foo).and_return(1)
           class RSpec::SampleRspecTestClass;end
           RSpec::SampleRspecTestClass.foo.should == 1
