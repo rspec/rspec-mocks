rspec/rspec-core

commit deec990c93ad857c920a726b1e6633ac375bef50

Fix use of const_defined? and const_get so it ignores top-level constants.
I didn't realize this previously, but these methods can pick up a top-level
constant when you don't intend it (e.g. ::Hash when checking MyGem.const_defined?("Hash")).

lib/rspec/mocks/stub_const.rb
@@ -4,15 +4,54 @@ module RSpec
     # constant stubbing.
     # @api private
     module RecursiveConstMethods
+      # We only want to consider constants that are defined directly on a
+      # particular module, and not include top-level/inherited constants.
+      # Unfortunately, the constant API changed between 1.8 and 1.9, so
+      # we need to conditionally define methods to ignore the top-level/inherited
+      # constants.
+      #
+      # Given `class A; end`:
+      #
+      # On 1.8:
+      #   - A.const_get("Hash") # => ::Hash
+      #   - A.const_defined?("Hash") # => false
+      #   - Neither method accepts the extra `inherit` argument
+      # On 1.9:
+      #   - A.const_get("Hash") # => ::Hash
+      #   - A.const_defined?("Hash") # => true
+      #   - A.const_get("Hash", false) # => raises NameError
+      #   - A.const_defined?("Hash", false) # => false
+      if Module.method(:const_defined?).arity == 1
+        def const_defined_on?(mod, const_name)
+          mod.const_defined?(const_name)
+        end
+
+        def get_const_defined_on(mod, const_name)
+          if const_defined_on?(mod, const_name)
+            return mod.const_get(const_name)
+          end
+
+          raise NameError, "uninitialized constant #{mod.name}::#{const_name}"
+        end
+      else
+        def const_defined_on?(mod, const_name)
+          mod.const_defined?(const_name, false)
+        end
+
+        def get_const_defined_on(mod, const_name)
+          mod.const_get(const_name, false)
+        end
+      end
+
       def recursive_const_get(const_name)
-        const_name.split('::').inject(Object) { |mod, name| mod.const_get name }
+        const_name.split('::').inject(Object) { |mod, name| get_const_defined_on(mod, name) }
       end
 
       def recursive_const_defined?(const_name)
         const_name.split('::').inject([Object, '']) do |(mod, full_name), name|
           yield(full_name, name) if block_given? && !mod.is_a?(Module)
-          return false unless mod.const_defined?(name)
-          [mod.const_get(name), [mod, name].join('::')]
+          return false unless const_defined_on?(mod, name)
+          [get_const_defined_on(mod, name), [mod, name].join('::')]
         end
       end
     end
@@ -139,7 +178,7 @@ module RSpec
       class DefinedConstantReplacer < BaseStubber
         def stub
           @context = recursive_const_get(@context_parts.join('::'))
-          @original_value = @context.const_get(@const_name)
+          @original_value = get_const_defined_on(@context, @const_name)
 
           constants_to_transfer = verify_constants_to_transfer!
 
@@ -160,7 +199,7 @@ module RSpec
 
         def transfer_nested_constants(constants)
           constants.each do |const|
-            @stubbed_value.const_set(const, original_value.const_get(const))
+            @stubbed_value.const_set(const, get_const_defined_on(original_value, const))
           end
         end
 
@@ -202,9 +241,9 @@ module RSpec
         def stub
           remaining_parts = @context_parts.dup
           @deepest_defined_const = @context_parts.inject(Object) do |klass, name|
-            break klass unless klass.const_defined?(name)
+            break klass unless const_defined_on?(klass, name)
             remaining_parts.shift
-            klass.const_get(name)
+            get_const_defined_on(klass, name)
           end
 
           context = remaining_parts.inject(@deepest_defined_const) do |klass, name|

spec/rspec/mocks/stub_const_spec.rb
@@ -177,6 +177,10 @@ module RSpec
           it_behaves_like "loaded constant stubbing", "TestClass::Nested"
         end
 
+        context 'for an unloaded constant with nested name that matches a top-level constant' do
+          it_behaves_like "unloaded constant stubbing", "TestClass::Hash"
+        end
+
         context 'for a loaded deeply nested constant' do
           it_behaves_like "loaded constant stubbing", "TestClass::Nested::NestedEvenMore"
         end

rspec/rspec-mocks

commit 39ae1f3d2c555781071f8537753697a5ffc3c997

Fix transfer_nested_constants option of stub_const.
My changes in deec990c93ad857c920a726b1e6633ac375bef50 caused it to blow up in the face of inherited constants.

lib/rspec/mocks/stub_const.rb
@@ -10,17 +10,22 @@ module RSpec
       # we need to conditionally define methods to ignore the top-level/inherited
       # constants.
       #
-      # Given `class A; end`:
+      # Given:
+      #   class A; B = 1; end
+      #   class C < A; end
       #
       # On 1.8:
-      #   - A.const_get("Hash") # => ::Hash
-      #   - A.const_defined?("Hash") # => false
-      #   - Neither method accepts the extra `inherit` argument
+      #   - C.const_get("Hash") # => ::Hash
+      #   - C.const_defined?("Hash") # => false
+      #   - C.constants # => ["A"]
+      #   - None of these methods accept the extra `inherit` argument
       # On 1.9:
-      #   - A.const_get("Hash") # => ::Hash
-      #   - A.const_defined?("Hash") # => true
-      #   - A.const_get("Hash", false) # => raises NameError
-      #   - A.const_defined?("Hash", false) # => false
+      #   - C.const_get("Hash") # => ::Hash
+      #   - C.const_defined?("Hash") # => true
+      #   - C.const_get("Hash", false) # => raises NameError
+      #   - C.const_defined?("Hash", false) # => false
+      #   - C.constants # => [:A]
+      #   - C.constants(false) #=> []
       if Module.method(:const_defined?).arity == 1
         def const_defined_on?(mod, const_name)
           mod.const_defined?(const_name)
@@ -33,6 +38,10 @@ module RSpec
 
           raise NameError, "uninitialized constant #{mod.name}::#{const_name}"
         end
+
+        def constants_defined_on(mod)
+          mod.constants.select { |c| const_defined_on?(mod, c) }
+        end
       else
         def const_defined_on?(mod, const_name)
           mod.const_defined?(const_name, false)
@@ -40,6 +49,10 @@ module RSpec
 
         def get_const_defined_on(mod, const_name)
           mod.const_get(const_name, false)
+        end
+
+        def constants_defined_on(mod)
+          mod.constants(false)
         end
       end
 
@@ -217,10 +230,10 @@ module RSpec
 
           if @transfer_nested_constants.is_a?(Array)
             @transfer_nested_constants = @transfer_nested_constants.map(&:to_s) if RUBY_VERSION == '1.8.7'
-            undefined_constants = @transfer_nested_constants - @original_value.constants
+            undefined_constants = @transfer_nested_constants - constants_defined_on(@original_value)
 
             if undefined_constants.any?
-              available_constants = @original_value.constants - @transfer_nested_constants
+              available_constants = constants_defined_on(@original_value) - @transfer_nested_constants
               raise ArgumentError,
                 "Cannot transfer nested constant(s) #{undefined_constants.join(' and ')} " +
                 "for #{@full_constant_name} since they are not defined. Did you mean " +
@@ -229,7 +242,7 @@ module RSpec
 
             @transfer_nested_constants
           else
-            @original_value.constants
+            constants_defined_on(@original_value)
           end
         end
       end

spec/rspec/mocks/stub_const_spec.rb
@@ -12,6 +12,10 @@ class TestClass
   end
 end
 
+class TestSubClass < TestClass
+  P = :p
+end
+
 module RSpec
   module Mocks
     describe "Constant Stubbing" do
@@ -119,6 +123,24 @@ module RSpec
             stub::M.should eq(:m)
             stub::N.should eq(:n)
             stub::Nested.should be(tc_nested)
+          end
+
+          it 'does not transfer nested constants that are inherited from a superclass' do
+            stub = Module.new
+            stub_const("TestSubClass", stub, :transfer_nested_constants => true)
+            stub::P.should eq(:p)
+            defined?(stub::M).should be_false
+            defined?(stub::N).should be_false
+          end
+
+          it 'raises an error when asked to transfer a nested inherited constant' do
+            original_tsc = TestSubClass
+
+            expect {
+              stub_const("TestSubClass", Module.new, :transfer_nested_constants => [:M])
+            }.to raise_error(ArgumentError)
+
+            TestSubClass.should be(original_tsc)
           end
 
           it 'allows nested constants to be selectively transferred to a stub module' do