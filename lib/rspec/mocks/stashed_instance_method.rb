8dee2ec872107b9780531145590194124dfdea85
Andy Lindeman <alindeman@gmail.com>
2012-09-06 18:27:03 -0400

    Correctly unstubs methods stubbed with `#any_instance`
    * Instances stubbed with `#any_instance` would not be usable after the
      test finished because `MethodDouble` would stash the implementation of
      the method already overridden by `AnyInstance::Recorder`. When the
      test finished, that implementation would be restored on the object's
      singleton class, and any future calls to it would blow up with a stack
      overflow.
    * This fix only stashes methods if they are defined on the object's
      singleton class to begin with; `AnyInstance::Recorder` defines a
      method on the object's class so that method will not be stashed.
    * If there is no method on the object's singleton class, RSpec can
      safely define one there without stashing the original implementation.
      At the end of the test, the method is simply removed entirely from the
      singleton class. Any original implementation defined in the object's
      ancestor chain will show through again.
    * This issue cannot be fixed on MRI 1.8.6 because it does not support
      `Method#owner`. However, `#any_instance` itself is not supported on
      1.8.6 for the same reason. The fix should not negatively affect 1.8.6,
      though, because the fallback behavior is to stash the method in all
      cases (which was the original behavior).
    
    * This commit also refactors the stashing behavior out into its own
      object. While not explicitly necessary, it helped me reason about the
      fix much easier than when all the responsibility was in `MethodDouble`
      (which also has other responsibilities).
    
    * Fixes #167

---
+++ lib/rspec/mocks/stashed_instance_method.rb
@@ -0,0 +1,54 @@
+# @private
+class StashedInstanceMethod
+  def initialize(klass, method)
+    @klass = klass
+    @method = method
+
+    @method_is_stashed = false
+  end
+
+  # @private
+  def stash
+    return if !method_defined_directly_on_klass? || @method_is_stashed
+
+    @klass.class_eval <<-EOF, __FILE__, __LINE__ + 1
+      alias_method :#{stashed_method_name}, :#{@method}
+    EOF
+    @method_is_stashed = true
+  end
+
+  private
+
+  # @private
+  def method_defined_directly_on_klass?
+    if @klass.method_defined?(@method) || @klass.private_method_defined?(@method)
+      method_obj = @klass.instance_method(@method)
+      if method_obj && method_obj.respond_to?(:owner)
+        method_obj.owner == @klass
+      else
+        # On 1.8.6, which does not support Method#owner, we have no choice but
+        # to assume it's defined on the klass even if it may be defined on
+        # a superclass.
+        true
+      end
+    end
+  end
+
+  # @private
+  def stashed_method_name
+    "obfuscated_by_rspec_mocks__#{@method}"
+  end
+
+  public
+
+  # @private
+  def restore
+    return unless @method_is_stashed
+
+    @klass.class_eval <<-EOF, __FILE__, __LINE__ + 1
+      alias_method :#{@method}, :#{stashed_method_name}
+      remove_method :#{stashed_method_name}
+    EOF
+    @method_is_stashed = false
+  end
+end
