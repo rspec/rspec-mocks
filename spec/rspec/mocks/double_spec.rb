843a40f4483be9888ba03a415468be99182f0b4a
Michi Huber <michi.huber@gmail.com>
2013-03-01 11:32:11 +0100

    output warning if #mock or #stub are used to instantiate test doubles

--- spec/rspec/mocks/double_spec.rb
+++ spec/rspec/mocks/double_spec.rb
@@ -4,9 +4,21 @@ describe "double" do
   it "is an alias for stub and mock" do
     expect(double()).to be_a(RSpec::Mocks::Mock)
   end
-  
+
   it "uses 'Double' in failure messages" do
     double = double('name')
     expect {double.foo}.to raise_error(/Double "name" received/)
+  end
+
+  describe "deprecated aliases" do
+    it "warns if #stub is used" do
+      RSpec::Mocks.should_receive(:warn_deprecation).with(/DEPRECATION: use #double instead of #stub/)
+      stub("TestDouble")
+    end
+
+    it "warns if #mock is used" do
+      RSpec::Mocks.should_receive(:warn_deprecation).with(/DEPRECATION: use #double instead of #mock/)
+      mock("TestDouble")
+    end
   end
 end
