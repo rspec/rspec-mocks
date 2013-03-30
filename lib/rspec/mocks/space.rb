b2204955f7c474fca4aaeb44f3438ec262f54a13
Myron Marston <myron.marston@gmail.com>
2013-03-30 15:24:00 -0700

    Play with seperating all proxies from current proxies.

--- lib/rspec/mocks/space.rb
+++ lib/rspec/mocks/space.rb
@@ -6,11 +6,12 @@ module RSpec
 
       def initialize
         @proxies = {}
+        @current_example_proxies = Set.new
       end
 
       def verify_all
-        proxies.each_value do |object|
-          object.verify
+        @current_example_proxies.each do |proxy|
+          proxy.verify
         end
 
         AnyInstance.verify_all
@@ -20,11 +21,11 @@ module RSpec
         ConstantMutator.reset_all
         AnyInstance.reset_all
 
-        proxies.each_value do |object|
-          object.reset
+        @current_example_proxies.each do |proxy|
+          proxy.reset
         end
 
-        proxies.clear
+        @current_example_proxies.clear
         expectation_ordering.clear
       end
 
@@ -33,13 +34,16 @@ module RSpec
       end
 
       def proxy_for(object)
-        proxies.fetch(object.object_id) do
+        proxy = proxies.fetch(object.object_id) do
           proxies[object.object_id] = if TestDouble === object
                                         object.__build_mock_proxy
                                       else
                                         Proxy.new(object)
                                       end
         end
+
+        @current_example_proxies << proxy
+        proxy
       end
 
       alias ensure_registered proxy_for
