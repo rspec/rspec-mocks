c864e0a530add6638e404b84d1608652f3882060
Myron Marston <myron.marston@gmail.com>
2013-04-01 22:32:27 -0700

    Candidate fix for #258.

--- lib/rspec/mocks/method_double.rb
+++ lib/rspec/mocks/method_double.rb
@@ -3,7 +3,7 @@ module RSpec
     # @private
     class MethodDouble
       # @private
-      attr_reader :method_name, :object, :expectations, :stubs
+      attr_reader :method_name, :object, :expectations, :stubs, :original_method
 
       # @private
       def initialize(object, method_name, proxy)
@@ -13,8 +13,24 @@ module RSpec
 
         @method_stasher = InstanceMethodStasher.new(object_singleton_class, @method_name)
         @method_is_proxied = false
+        @original_method = find_original_method
         @expectations = []
         @stubs = []
+      end
+
+      def find_original_method
+        method_handle_name = if any_instance_class_recorder_observing_method?(@object.class)
+          recorder = ::RSpec::Mocks.any_instance_recorder_for(@object.class)
+          recorder.build_alias_method_name(@method_name)
+        else
+          @method_name
+        end
+
+        ::RSpec::Mocks.method_handle_for(@object, method_handle_name)
+      rescue NameError
+        Proc.new do |*args, &block|
+          @object.__send__(:method_missing, @method_name, *args, &block)
+        end
       end
 
       # @private
@@ -30,46 +46,6 @@ module RSpec
         end
       end
 
-      class ProcWithBlock < Struct.new(:object, :method_name)
-
-        def call(*args, &block)
-          self.object.__send__(:method_missing, self.method_name, *args, &block)
-        end
-
-      end
-
-      # @private
-      def original_method
-        if @method_stasher.method_is_stashed?
-          # Example: a singleton method defined on @object
-          ::RSpec::Mocks.method_handle_for(@object, @method_stasher.stashed_method_name)
-        elsif meth = original_unrecorded_any_instance_method
-          # Example: a method that has been mocked through
-          #   klass.any_instance.should_receive(:msg).and_call_original
-          # any_instance.should_receive(:msg) causes the method to be
-          # replaced with a proxy method, and then `and_call_original`
-          # is recorded and played back on the object instance. We need
-          # special handling here to get a handle on the original method
-          # object rather than the proxy method.
-          meth
-        else
-          # Example: an instance method defined on one of @object's ancestors.
-          original_method_from_ancestry
-        end
-      rescue NameError
-        # We have no way of knowing if the object's method_missing
-        # will handle this message or not...but we can at least try.
-        # If it's not handled, a `NoMethodError` will be raised, just
-        # like normally.
-        ProcWithBlock.new(@object,@method_name)
-      end
-
-      def original_unrecorded_any_instance_method
-        return nil unless any_instance_class_recorder_observing_method?(@object.class)
-        alias_name = ::RSpec::Mocks.any_instance_recorder_for(@object.class).build_alias_method_name(@method_name)
-        @object.method(alias_name)
-      end
-
       def any_instance_class_recorder_observing_method?(klass)
         return true if ::RSpec::Mocks.any_instance_recorder_for(klass).already_observing?(@method_name)
         superklass = klass.superclass
@@ -77,72 +53,6 @@ module RSpec
         any_instance_class_recorder_observing_method?(superklass)
       end
 
-      our_singleton_class = class << self; self; end
-      if our_singleton_class.ancestors.include? our_singleton_class
-        # In Ruby 2.1, ancestors include the correct ancestors, including the singleton classes
-        def original_method_from_ancestry
-          # Lookup in the ancestry, skipping over the singleton class itself
-          original_method_from_ancestor(object_singleton_class.ancestors.drop(1))
-        end
-      else
-        # @private
-        def original_method_from_ancestry
-          original_method_from_ancestor(object_singleton_class.ancestors)
-        rescue NameError
-          raise unless @object.respond_to?(:superclass)
-
-          # Example: a singleton method defined on @object's superclass.
-          #
-          # Note: we have to give precedence to instance methods
-          # defined on @object's class, because in a case like:
-          #
-          # `klass.should_receive(:new).and_call_original`
-          #
-          # ...we want `Class#new` bound to `klass` (which will return
-          # an instance of `klass`), not `klass.superclass.new` (which
-          # would return an instance of `klass.superclass`).
-          original_method_from_superclass
-        end
-      end
-
-      def original_method_from_ancestor(ancestors)
-        klass, *rest = ancestors
-        klass.instance_method(@method_name).bind(@object)
-      rescue NameError
-        raise if rest.empty?
-        original_method_from_ancestor(rest)
-      end
-
-      if RUBY_VERSION.to_f > 1.8
-        # @private
-        def original_method_from_superclass
-          @object.superclass.
-                  singleton_class.
-                  instance_method(@method_name).
-                  bind(@object)
-        end
-      else
-        # Our implementation for 1.9 (above) causes an error on 1.8:
-        # TypeError: singleton method bound for a different object
-        #
-        # This doesn't work quite right in all circumstances but it's the
-        # best we can do.
-        # @private
-        def original_method_from_superclass
-          ::Kernel.warn <<-WARNING.gsub(/^ +\|/, '')
-            |
-            |WARNING: On ruby 1.8, rspec-mocks is unable to bind the original
-            |`#{@method_name}` method to your partial mock object (#{@object})
-            |for `and_call_original`. The superclass's `#{@method_name}` is being
-            |used instead; however, it may not work correctly when executed due
-            |to the fact that `self` will be #{@object.superclass}, not #{@object}.
-            |
-            |Called from: #{CallerFilter.first_non_rspec_line}
-          WARNING
-
-          @object.superclass.method(@method_name)
-        end
-      end
       # @private
       def object_singleton_class
         class << @object; self; end
--- spec/rspec/mocks/and_call_original_spec.rb
+++ spec/rspec/mocks/and_call_original_spec.rb
@@ -26,6 +26,12 @@ describe "and_call_original" do
       expect(instance.meth_1).to eq(:original)
     end
 
+    it 'ignores prior declared stubs' do
+      instance.stub(:meth_1).and_return(:stubbed_value)
+      instance.should_receive(:meth_1).and_call_original
+      expect(instance.meth_1).to eq(:original)
+    end
+
     it 'passes args and blocks through to the original method' do
       instance.should_receive(:meth_2).and_call_original
       value = instance.meth_2(:submitted_arg) { |a, b| [a, b] }
@@ -104,33 +110,16 @@ describe "and_call_original" do
       end
     end
 
-    if RUBY_VERSION.to_f > 1.8
-      it 'works for class methods defined on a superclass' do
-        subclass = Class.new(klass)
-        subclass.should_receive(:new_instance).and_call_original
-        expect(subclass.new_instance).to be_a(subclass)
-      end
+    it 'works for class methods defined on a superclass' do
+      subclass = Class.new(klass)
+      subclass.should_receive(:new_instance).and_call_original
+      expect(subclass.new_instance).to be_a(subclass)
+    end
 
-      it 'works for class methods defined on a grandparent class' do
-        sub_subclass = Class.new(Class.new(klass))
-        sub_subclass.should_receive(:new_instance).and_call_original
-        expect(sub_subclass.new_instance).to be_a(sub_subclass)
-      end
-    else
-      it 'attempts to work for class methods defined on a superclass but ' +
-         'executes the method with `self` as the superclass' do
-        ::Kernel.stub(:warn)
-        subclass = Class.new(klass)
-        subclass.should_receive(:new_instance).and_call_original
-        expect(subclass.new_instance).to be_an_instance_of(klass)
-       end
-
-      it 'prints a warning to notify users that `self` will not be correct' do
-        subclass = Class.new(klass)
-        ::Kernel.should_receive(:warn).with(/may not work correctly/)
-        subclass.should_receive(:new_instance).and_call_original
-        subclass.new_instance
-      end
+    it 'works for class methods defined on a grandparent class' do
+      sub_subclass = Class.new(Class.new(klass))
+      sub_subclass.should_receive(:new_instance).and_call_original
+      expect(sub_subclass.new_instance).to be_a(sub_subclass)
     end
 
     it 'works for class methods defined on the Class class' do
