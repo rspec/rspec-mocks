edece8ceca811fa6a972df47d0f54823228bf15d
Jon Rowe <hello@jonrowe.co.uk>
2013-04-24 09:23:41 +1000

    forcibly reset warnings

--- spec/rspec/mocks/nil_expectation_warning_spec.rb
+++ spec/rspec/mocks/nil_expectation_warning_spec.rb
@@ -60,6 +60,7 @@ module RSpec
       it 'doesnt error when marshalled' do
         allow_message_expectations_on_nil
         expect(Marshal.dump(nil)).to eq Marshal.dump_without_mocks(nil)
+        Proxy.warn_about_expectations_on_nil = true
       end
     end
   end
