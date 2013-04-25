675c52e66ca30f1e0075a79fe361379e0f7e661e
Jon Rowe <hello@jonrowe.co.uk>
2013-04-25 15:06:44 +1000

    refactor ifs back into methods

--- lib/rspec/mocks/proxy_for_nil.rb
+++ lib/rspec/mocks/proxy_for_nil.rb
@@ -8,30 +8,27 @@ module RSpec
         super nil
       end
       attr_accessor :warn_about_expectations
+      alias warn_about_expectations? warn_about_expectations
 
       def add_message_expectation(location, method_name, opts={}, &block)
-        warn_when_warning_about_expectations method_name
+        warn(method_name) if warn_about_expectations?
         super
       end
 
       def add_negative_message_expectation(location, method_name, &implementation)
-        warn_when_warning_about_expectations method_name
+        warn(method_name) if warn_about_expectations?
         super
       end
 
       def add_stub(location, method_name, opts={}, &implementation)
-        warn_when_warning_about_expectations method_name
+        warn(method_name) if warn_about_expectations?
         super
       end
 
       private
 
-      def warn_when_warning_about_expectations method_name
-        warn( method_name ) if warn_about_expectations
-      end
-
       def warn method_name
-        Kernel.warn("An expectation of :#{method_name} was set on nil. Called from #{caller[3]}. Use allow_message_expectations_on_nil to disable warnings.")
+        Kernel.warn("An expectation of :#{method_name} was set on nil. Called from #{caller[2]}. Use allow_message_expectations_on_nil to disable warnings.")
       end
 
     end
