14da850547a998dafc6f38b1b6eaa61d82105b6e
Sam Phippen <samphippen@googlemail.com>
2013-05-30 01:52:58 +0100

    Add an array_including argument matcher
    Closes #295.
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ spec/rspec/mocks/array_including_matcher_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+module RSpec
+  module Mocks
+    module ArgumentMatchers
+      describe ArrayIncludingMatcher do
+        it "describes itself properly" do
+          expect(ArrayIncludingMatcher.new([1, 2, 3]).description).to eq "array_including(1,2,3)"
+        end
+
+        context "passing" do
+          it "matches the same array" do
+            expect(array_including(1, 2, 3)).to eq([1, 2, 3])
+          end
+
+          it "matches the same array,  specified without square brackets" do
+            expect(array_including(1, 2, 3)).to eq([1, 2, 3])
+          end
+
+          it "matches the same array,  which includes nested arrays" do
+            expect(array_including([1, 2], 3, 4)).to eq([[1, 2], 3, 4])
+          end
+
+          it "works with duplicates in expected" do
+            expect(array_including(1, 1, 2, 3)).to eq([1, 2, 3])
+          end
+
+          it "works with duplicates in actual" do
+            expect(array_including(1, 2, 3)).to eq([1, 1, 2, 3])
+          end
+        end
+
+        context "failing" do
+          it "fails when not all the entries in the expected are present" do
+            expect(array_including(1,2,3,4,5)).not_to eq([1,2])
+          end
+        end
+      end
+    end
+  end
+end
