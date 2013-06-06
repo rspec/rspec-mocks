988b6b1bcf3500d4fcf4815e5595ad5f883a8451
Jon Rowe <hello@jonrowe.co.uk>
2013-06-06 12:06:55 +1000

    tweak setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,11 @@ begin
 rescue LoadError
 end
 
+require 'simplecov' if RUBY_VERSION.to_f > 1.8
 require 'coveralls'
-Coveralls.wear!
+Coveralls.wear! do
+  add_filter '/bundle/'
+end
 
 RSpec::Matchers.define :include_method do |expected|
   match do |actual|
