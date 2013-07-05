f820e6c2f24fafdf8fb6a684d204f59df6ae2b39
Sam Phippen <samphippen@googlemail.com>
2013-07-05 02:03:10 +0200

    Remove any_number_of_times_spec now that the method is gone.

--- spec/rspec/mocks/any_number_of_times_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe "AnyNumberOfTimes" do
-  it "is gone" do
-    expect(RSpec::Mocks::MessageExpectation.instance_methods).not_to include(:any_number_of_times)
-  end
-end
