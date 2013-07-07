0731ddf87c9858bcfa6aefb1861d83f2915c5dc0
Myron Marston <myron.marston@gmail.com>
2013-07-07 15:52:15 -0700

    Deprecate reliance on a double's null-ness persisting between examples.

--- spec/rspec/mocks/null_object_mock_spec.rb
+++ spec/rspec/mocks/null_object_mock_spec.rb
@@ -88,6 +88,20 @@ module RSpec
         # returns an int value from #to_int.
         expect(("%i" % @double)).to eq("0")
       end
+
+      it "preserves its nullness to subsequent examples to " +
+         "maintain compatibility with <= 2.13" do
+        RSpec::Mocks.teardown
+        allow(RSpec).to receive(:deprecate)
+        expect(@double).to be_null_object
+        expect { @double.some.long.message.chain }.not_to raise_error
+      end
+
+      it 'prints a deprecation warning when a double is re-used between examples' do
+        RSpec::Mocks.teardown
+        expect(RSpec).to receive(:deprecate).with(/null-ness/)
+        expect { @double.some.long.message.chain }.not_to raise_error
+      end
     end
 
     describe "#as_null_object" do
