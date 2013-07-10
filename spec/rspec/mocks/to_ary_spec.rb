7018a108fdbf2db5c64fe2a44dddf1e3b832fba3
Jon Rowe <hello@jonrowe.co.uk>
2013-07-11 07:59:06 +1000

    1.8.7 has a default implementation on top

--- spec/rspec/mocks/to_ary_spec.rb
+++ spec/rspec/mocks/to_ary_spec.rb
@@ -2,10 +2,6 @@ require "spec_helper"
 
 describe "a double receiving to_ary" do
   shared_examples "to_ary" do
-    it "doesn't respond" do
-      expect(obj).not_to respond_to(:to_ary)
-    end
-
     it "can be overridden with a stub" do
       obj.stub(:to_ary) { :non_nil_value }
       expect(obj.to_ary).to be(:non_nil_value)
@@ -26,6 +22,14 @@ describe "a double receiving to_ary" do
     let(:obj) { double('obj').as_null_object }
     include_examples "to_ary"
 
+    it "does respond to to_ary" do
+      expect(obj).to respond_to(:to_ary)
+    end
+
+    it "does respond to to_a" do
+      expect(obj).to respond_to(:to_a)
+    end
+
     it "returns nil" do
       expect(obj.to_ary).to eq nil
     end
@@ -35,7 +39,15 @@ describe "a double receiving to_ary" do
     let(:obj) { double('obj') }
     include_examples "to_ary"
 
-    it "raises NoMethodError" do
+    it "doesn't respond to to_ary" do
+      expect(obj).not_to respond_to(:to_ary)
+    end
+
+    it "doesn't respond to to_a", :if => ( RUBY_VERSION.to_f > 1.8 ) do
+      expect(obj).not_to respond_to(:to_a)
+    end
+
+    it "raises " do
       expect { obj.to_ary }.to raise_error(NoMethodError)
     end
   end
