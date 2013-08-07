0902a18ad36f8d872d6feba5b965247de889f4a6
Sam Phippen <samphippen@googlemail.com>
2013-08-07 12:16:01 +0100

    Improve the warning message for any_instance block expectations.
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- lib/rspec/mocks/any_instance/expectation_chain.rb
+++ lib/rspec/mocks/any_instance/expectation_chain.rb
@@ -44,11 +44,21 @@ module RSpec
         end
 
         def warn_about_receiver_passing_if_necessary
-          Kernel.warn(<<MSG
-`expect_any_instance_of(...).to receive(:message) { ... }` blocks will get the
-receiving instance in 3.0. please explicitly set:
-`RSpec::Mocks.configuration.yield_receiver_to_any_instance_implementation_blocks = true`
-in your spec helper and fix any failing specs.
+          RSpec.warn_deprecation(<<MSG
+In RSpec 3, `any_instance` implementation blocks will be yielded the receiving
+instance as the first block argument to allow the implementation block to use
+the state of the receiver.  To maintain compatibility with RSpec 3 you need to
+either set rspec-mocks' `yield_receiver_to_any_instance_implementation_blocks`
+config option to `false` OR set it to `true` and update your `any_instance`
+implementation blocks to account for the first block argument being the receiving instance.
+
+To set the config option, use a snippet like:
+
+RSpec.configure do |rspec|
+  rspec.mock_with :rspec do |mocks|
+    mocks.yield_receiver_to_any_instance_implementation_blocks = false
+  end
+end
 MSG
           ) if RSpec::Mocks.configuration.should_warn_about_any_instance_blocks?
         end
