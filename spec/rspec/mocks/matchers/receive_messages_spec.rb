9ef017d34a03dbd437d46f4821075a499908915c
Jon Rowe <hello@jonrowe.co.uk>
2013-08-10 00:31:38 +0800

    refactor specs to use verify and include any_instance_of examples

--- spec/rspec/mocks/matchers/receive_messages_spec.rb
+++ spec/rspec/mocks/matchers/receive_messages_spec.rb
@@ -11,11 +11,6 @@ module RSpec
         expect(obj.b).to eq 2
       end
 
-      it "allows single expectations" do
-        allow(obj).to receive_messages(:a => 1)
-        expect(obj.a).to eq 1
-      end
-
       it "complains if a block is given" do
         expect do
           allow(obj).to receive_messages(:a => 1) { "implementation" }
@@ -23,53 +18,50 @@ module RSpec
       end
     end
 
-    describe "expect(...).to receive_messages(:a => 1, :b => 2)" do
-      let(:reporter) { RSpec::Core::Reporter.new }
+    describe "allow_any_instance_of(...).to receive_messages(:a => 1, :b => 2)" do
+      let(:obj) { Object.new }
 
-      it "sets up multiple expectations" do
-        expect(reporter).to receive(:example_passed).with("will pass")
-        expect(reporter).to receive(:example_failed).with("will fail")
-
-        example_group = ::RSpec::Core::ExampleGroup.describe do
-          before do
-            obj = double "Object"
-            expect(obj).to receive_messages(:a => 1, :b => 2)
-          end
-
-          it "will pass" do
-            obj.a && obj.b
-          end
-
-          it "will fail" do
-            obj.a
-          end
-        end
-        example_group.run reporter
+      it "allows the object to respond to multiple messages" do
+        allow_any_instance_of(Object).to receive_messages(:a => 1, :b => 2)
+        expect(obj.a).to eq 1
+        expect(obj.b).to eq 2
       end
 
-      it "allows single expectations" do
-        expect(reporter).to receive(:example_passed).with("will pass")
-        expect(reporter).to receive(:example_failed).with("will fail")
+      it "complains if a block is given" do
+        expect do
+          allow_any_instance_of(Object).to receive_messages(:a => 1) { "implementation" }
+        end.to raise_error "Implementation blocks arn't supported with `receive_messages`"
+      end
+    end
 
-        example_group = ::RSpec::Core::ExampleGroup.describe do
-          before do
-            obj = double "Object"
-            expect(obj).to receive_messages(:a => 1)
-          end
+    describe "expect(...).to receive_messages(:a => 1, :b => 2)" do
+      let(:obj) { double "Object" }
 
-          it "will pass" do
-            obj.a
-          end
-
-          it "will fail" do
-          end
-        end
-        example_group.run reporter
+      it "sets up multiple expectations" do
+        expect(obj).to receive_messages(:a => 1, :b => 2)
+        obj.a
+        expect { RSpec::Mocks.space.verify_all }.to raise_error RSpec::Mocks::MockExpectationError
       end
 
       it "complains if a block is given" do
         expect do
           expect(double).to receive_messages(:a => 1) { "implementation" }
+        end.to raise_error "Implementation blocks arn't supported with `receive_messages`"
+      end
+    end
+
+    describe "expect_any_instance_of(...).to receive_messages(:a => 1, :b => 2)" do
+      let(:obj) { Object.new }
+
+      it "sets up multiple expectations" do
+        expect_any_instance_of(Object).to receive_messages(:a => 1, :b => 2)
+        obj.a
+        expect { RSpec::Mocks.space.verify_all }.to raise_error RSpec::Mocks::MockExpectationError
+      end
+
+      it "complains if a block is given" do
+        expect do
+          expect_any_instance_of(Object).to receive_messages(:a => 1) { "implementation" }
         end.to raise_error "Implementation blocks arn't supported with `receive_messages`"
       end
     end
