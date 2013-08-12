8611473599ed49f2115edac02f29641e39d67633
Jon Rowe <hello@jonrowe.co.uk>
2013-08-12 22:06:26 +0100

    refactor receive messages to use the simple stub/expectation when available

--- lib/rspec/mocks/matchers/receives_messages.rb
+++ lib/rspec/mocks/matchers/receives_messages.rb
@@ -4,33 +4,55 @@ module RSpec
       class ReceivesMessages
 
         def initialize(message_value_hash)
-          @receivers = message_value_hash.map do |method_name, value|
-            Matchers::Receive.new(method_name, proc { value })
+          @message_value_hash = message_value_hash
+        end
+
+        def setup_expectation(subject, &block)
+          map_to proxy_on(subject), :add_simple_expectation
+        end
+        alias matches?                   setup_expectation
+        alias does_not_match?            setup_expectation
+        alias setup_negative_expectation setup_expectation
+        alias setup_allowance            setup_expectation
+
+        def setup_allowance(subject)
+          map_to proxy_on(subject), :add_simple_stub
+        end
+
+        def setup_any_instance_expectation(subject)
+          map_to_as_chain any_instance_of(subject), :should_receive
+        end
+
+        def setup_any_instance_negative_expectation(subject)
+          map_to_as_chain any_instance_of(subject), :should_not_receive
+        end
+
+        def setup_any_instance_allowance(subject)
+          any_instance_of(subject).stub(@message_value_hash)
+        end
+
+      private
+
+        def proxy_on(subject)
+          ::RSpec::Mocks.proxy_for(subject)
+        end
+
+        def any_instance_of(subject)
+          ::RSpec::Mocks.any_instance_recorder_for(subject)
+        end
+
+        def map_to(host, method_name)
+          @message_value_hash.each do |message, value|
+            host.__send__(method_name, message.to_sym, value)
           end
         end
 
-        %w[
-          setup_expecation matches? setup_negative_expectation does_not_match?
-          setup_allowance setup_any_instance_expectation setup_any_instance_expectation
-          setup_any_instance_negative_expectation setup_any_instance_allowance
-        ].each do |method_name|
-          define_method(method_name) do |subject, &block|
-            @receivers.each do |receiver|
-              receiver.send(method_name, subject, &block)
-            end
+        def map_to_as_chain(host, method_name, *args)
+          @message_value_hash.each do |message, value|
+            host.__send__(method_name, message, *args).and_return(value)
           end
         end
 
-        MessageExpectation.public_instance_methods(false).each do |method_name|
-          next if method_defined?(method_name)
-
-          define_method(method_name) do |*args, &block|
-            @receivers.each do |receiver|
-              receiver.send(method_name, *args, &block)
-            end
-            self
-          end
-        end
       end
     end
   end
