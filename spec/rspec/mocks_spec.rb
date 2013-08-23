cd8ee94cafeafddda3692d59f256eaf8e4847aa9
Myron Marston <myron.marston@gmail.com>
2013-08-22 23:55:42 -0700

    Move caller_filter to `rspec/caller_filter`.
    - The caller_filter is generic for all of RSpec, not just
      rspec-mocks specific.
    - It's also at `rspec/caller_filter` in core (and will be
      in rspec-expectations soon). The idea is that any lib can
      require `rspec/caller_filter` and get this.
    - Add spec to verify the file stays up to date.

--- spec/rspec/mocks_spec.rb
+++ spec/rspec/mocks_spec.rb
@@ -55,4 +55,17 @@ describe RSpec::Mocks do
       expect(RSpec::Mocks.configuration).to be(RSpec::Mocks.configuration)
     end
   end
+
+  def file_contents_for(lib, filename)
+    path = $LOAD_PATH.find { |p| p.include?("/#{lib}/lib") }
+    file = File.join(path, filename)
+    File.read(file)
+  end
+
+  it 'has an up-to-date rspec/caller_filter file' do
+    mocks = file_contents_for("rspec-mocks", "rspec/caller_filter.rb")
+    core  = file_contents_for("rspec-core",  "rspec/caller_filter.rb")
+
+    expect(mocks).to eq(core)
+  end
 end
