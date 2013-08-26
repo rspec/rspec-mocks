f3d70e1ef20eceeb42d1f97099a5af020e4a8726
Myron Marston <myron.marston@gmail.com>
2013-08-25 22:41:35 -0700

    Update caller filter once more...

--- spec/rspec/mocks_spec.rb
+++ spec/rspec/mocks_spec.rb
@@ -64,10 +64,8 @@ describe RSpec::Mocks do
   end
 
   it 'has an up-to-date rspec/caller_filter file' do
-    pending "Holding off on this spec for now while this settles"
-
-    mocks = file_contents_for("mocks", "rspec/caller_filter.rb")
-    core  = file_contents_for("core",  "rspec/caller_filter.rb")
+    mocks = file_contents_for("mocks", "rspec/mocks/caller_filter.rb")
+    core  = file_contents_for("core",  "rspec/core/caller_filter.rb")
 
     expect(mocks).to eq(core)
   end
