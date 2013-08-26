3ac6f4e0155db12aa1ecbfe20d710e2d90c581fa
Myron Marston <myron.marston@gmail.com>
2013-08-25 23:23:05 -0700

    Correct inaccurate doc string.

--- spec/rspec/mocks_spec.rb
+++ spec/rspec/mocks_spec.rb
@@ -63,7 +63,7 @@ describe RSpec::Mocks do
     File.read(file)
   end
 
-  it 'has an up-to-date rspec/caller_filter file' do
+  it 'has an up-to-date caller_filter file' do
     mocks = file_contents_for("mocks", "rspec/mocks/caller_filter.rb")
     core  = file_contents_for("core",  "rspec/core/caller_filter.rb")
 
