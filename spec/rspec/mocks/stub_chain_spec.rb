db13dd315ecfa51ed45ac50ef91aa91704ddfa54
Sam Phippen <samphippen@googlemail.com>
2013-12-30 23:58:28 +0000

    Add a spec for implicit hash return

--- spec/rspec/mocks/stub_chain_spec.rb
+++ spec/rspec/mocks/stub_chain_spec.rb
@@ -37,8 +37,17 @@ module RSpec
       end
 
       context "with two methods in chain" do
-        it "accepts any number of arguments to the stubbed messages in the chain" do
+        it "accepts any number of arguments to the stubbed messages" do
           object.stub_chain(:msg1, :msg2).and_return(:return_value)
+
+          expect {
+            object.msg1("nonsense", :value).msg2("another", :nonsense, 3.0, "value")
+          }.not_to raise_error
+        end
+
+        it "accepts any number of arguments to the stubbed messages with a return value from a hash" do
+          object.stub_chain(:msg1, :msg2 => :return_value)
+
           expect {
             object.msg1("nonsense", :value).msg2("another", :nonsense, 3.0, "value")
           }.not_to raise_error
