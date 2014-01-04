0df48841c30d30251d8b6bf363f3953d337447d7
Myron Marston <myron.marston@gmail.com>
2014-01-03 16:35:09 -0800

    Update teardown spec.
    We're going to change how pure test doubles behave
    after teardown, so update this to focus on partial
    doubles (which will not be changed).

--- spec/rspec/mocks_spec.rb
+++ spec/rspec/mocks_spec.rb
@@ -14,13 +14,11 @@ describe RSpec::Mocks do
   end
 
   describe "::teardown" do
-    it "prevents further expectations from being set" do
-      foo = double
-      foo.should_receive(:bar)
-      RSpec::Mocks::teardown
-      expect do
-        foo.bar
-      end.to raise_error(RSpec::Mocks::OutsideOfExampleError)
+    it "resets method stubs" do
+      string = "foo"
+      allow(string).to receive(:bar)
+      RSpec::Mocks.teardown
+      expect { string.bar }.to raise_error(NoMethodError)
     end
   end
 
