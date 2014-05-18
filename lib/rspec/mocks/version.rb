9e018ebeb86284b810f2d7cdcb44eaf572dba5a1
Myron Marston <myron.marston@gmail.com>
2014-05-18 15:58:54 -0700

    Release 3.0.0.rc1

--- lib/rspec/mocks/version.rb
+++ lib/rspec/mocks/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec mocks.
     module Version
       # Version of RSpec mocks currently in use in SemVer format.
-      STRING = '3.0.0.beta2'
+      STRING = '3.0.0.rc1'
     end
   end
 end
