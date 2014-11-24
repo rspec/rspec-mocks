76b84ddf8c320cbed0251fb902c6974968f73c85
Cezary Baginski <cezary@chronomantic.net>
2014-11-24 20:19:13 +0100

    allow verifying doubles to use custom names in exceptions

--- lib/rspec/mocks/verifying_double.rb
+++ lib/rspec/mocks/verifying_double.rb
@@ -47,8 +47,17 @@ module RSpec
         __send__(name, *args, &block)
       end
 
-      def initialize(*args)
-        super
+      def initialize(doubled_module, *args)
+        @doubled_module = doubled_module
+
+        possible_name = args.first
+        name = if String === possible_name || Symbol === possible_name
+                 args.shift
+               else
+                 @description
+               end
+
+        super(name, *args)
         @__sending_message = nil
       end
     end
@@ -63,12 +72,8 @@ module RSpec
       include VerifyingDouble
 
       def initialize(doubled_module, *args)
-        @doubled_module = doubled_module
-
-        super(
-          "#{doubled_module.description} (instance)",
-          *args
-        )
+        @description = "#{doubled_module.description} (instance)"
+        super
       end
 
       def __build_mock_proxy(order_group)
@@ -95,8 +100,8 @@ module RSpec
     private
 
       def initialize(doubled_module, *args)
-        @doubled_module = doubled_module
-        super(doubled_module.description, *args)
+        @description = doubled_module.description
+        super
       end
 
       def __build_mock_proxy(order_group)
