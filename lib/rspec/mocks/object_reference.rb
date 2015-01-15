6493f2a09e4193ce11e2c4ce9493381a9f0b2c1b
Myron Marston <myron.marston@gmail.com>
2015-01-15 15:18:01 -0800

    Improve docs so they render properly with a YARD server.
    While we're at it, remove AnonymousModuleReference -- we don’t need it anymore!

--- lib/rspec/mocks/object_reference.rb
+++ lib/rspec/mocks/object_reference.rb
@@ -8,7 +8,7 @@ module RSpec
         case object_module_or_name
         when Module
           if anonymous_module?(object_module_or_name)
-            AnonymousModuleReference.new(object_module_or_name)
+            DirectObjectReference.new(object_module_or_name)
           else
             # Use a `NamedObjectReference` if it has a name because this
             # will use the original value of the constant in case it has
@@ -39,22 +39,26 @@ module RSpec
       private_class_method :anonymous_module?
     end
 
-    # Used when an object is passed to `object_double`.
+    # An implementation of rspec-mocks' reference interface.
+    # Used when an object is passed to {ExampleMethods#object_double}, or
+    # an anonymous class or module is passed to {ExampleMethods#instance_double}
+    # or {ExampleMethods#class_double}.
     # Represents a reference to that object.
+    # @see NamedObjectReference
     class DirectObjectReference
-      # @param object to which this refers to.
+      # @param object [Object] the object to which this refers
       def initialize(object)
         @object = object
       end
 
-      # The object's description (via `#inspect`).
-      # @return String
+      # @return [String] the object's description (via `#inspect`).
       def description
         @object.inspect
       end
 
       # Defined for interface parity with the other object reference
-      # implementations, it has no real relevance here.
+      # implementations. Raises an `ArgumentError` to indicate that `as_stubbed_const`
+      # is invalid when passing an object argument to `object_double`.
       def const_to_replace
         raise ArgumentError,
               "Can not perform constant replacement with an object."
@@ -62,14 +66,14 @@ module RSpec
 
       # The target of the verifying double (the object itself).
       #
-      # @return Object
+      # @return [Object]
       def target
         @object
       end
 
       # Always returns true for an object as the class is defined.
       #
-      # @return true
+      # @return [true]
       def defined?
         true
       end
@@ -81,67 +85,38 @@ module RSpec
       # This specific implementation always yields because direct references
       # are always loaded.
       #
-      # @yield object
+      # @yield [Object] the target of this reference.
       def when_loaded
         yield @object
       end
     end
 
-    # Used when an anonymous module is passed to `class_double` or
-    # `instance_double`. Represents a reference to that module.
-    class AnonymousModuleReference < DirectObjectReference
-      # Returns the constant name to replace with a double.
-      #
-      # @return String the constant name
-      def const_to_replace
-        @object.name
-      end
-
-      # Returns the module name.
-      #
-      # @return String the constant name
-      alias description const_to_replace
-
-      # The target of the verifying double (the module).
-      #
-      # @return Module
-      def target
-        @object
-      end
-    end
-
-    # Used when a string is passed to `class_double`, `instance_double`
-    # or `object_double`.
+    # An implementation of rspec-mocks' reference interface.
+    # Used when a string is passed to {ExampleMethods#object_double},
+    # and when a string, named class or named module is passed to
+    # {ExampleMethods#instance_double}, or {ExampleMethods#class_double}.
     # Represents a reference to the object named (via a constant lookup)
     # by the string.
+    # @see DirectObjectReference
     class NamedObjectReference
       # @param const_name [String] constant name
       def initialize(const_name)
         @const_name = const_name
       end
 
-      # Returns true if the named constant is defined, false otherwise.
-      #
-      # @return Boolean
+      # @return [Boolean] true if the named constant is defined, false otherwise.
       def defined?
         !!object
       end
 
-      # Returns the constant name to replace with a double.
-      #
-      # @return String the constant name
+      # @return [String] the constant name to replace with a double.
       def const_to_replace
         @const_name
       end
-
-      # Returns the object constant name.
-      #
-      # @return String the constant name
       alias description const_to_replace
 
-      # The target of the verifying double (the object).
-      #
-      # @return object
+      # @return [Object, nil] the target of the verifying double (the named object), or
+      #   nil if it is not defined.
       def target
         object
       end
@@ -150,7 +125,7 @@ module RSpec
       # to optionally run a bit of code only when a reference's target is
       # loaded.
       #
-      # @yield object
+      # @yield [Object] the target object
       def when_loaded
         yield object if object
       end
