8a0962a6b8a896b1f1f2806a1282f29cde7d2baa
Myron Marston <myron.marston@gmail.com>
2015-01-18 20:54:51 -0800

    Reword comment to not use “external” twice.
    Thanks to @waterlink for pointing this out.

--- lib/rspec/mocks/error_generator.rb
+++ lib/rspec/mocks/error_generator.rb
@@ -15,8 +15,9 @@ module RSpec
     # invoked.
     MockExpectationAlreadyInvokedError = Class.new(Exception)
 
-    # Raised for situations that RSpec cannot support due to external mutations
-    # made externally on arguments that RSpec is holding onto to use for later comparisons.
+    # Raised for situations that RSpec cannot support due to mutations made
+    # externally on arguments that RSpec is holding onto to use for later
+    # comparisons.
     CannotSupportArgMutationsError = Class.new(StandardError)
 
     # @private
