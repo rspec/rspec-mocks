08eb0f2a16f04c01e3231c16dd5a64bdec486776
Tim Wade <tim@majority.co>
2015-02-09 17:42:36 -0800

    Fix argument formatting for received.
    https://github.com/rspec/rspec-mocks/issues/890

--- lib/rspec/mocks/error_generator.rb
+++ lib/rspec/mocks/error_generator.rb
@@ -142,7 +142,7 @@ module RSpec
       def actual_method_call_args_description(count, args)
         method_call_args_description(args) ||
           if count > 0 && args.length > 0
-            " with arguments: #{args.inspect.gsub(/\A\[(.+)\]\z/, '(\1)')}"
+            " with arguments: #{format_args(*args)}"
           else
             ""
           end
