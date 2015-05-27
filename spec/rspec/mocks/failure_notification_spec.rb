bc3d0056337688940bd443ae4539912d3e3064b2
Myron Marston <myron.marston@gmail.com>
2015-05-26 22:16:42 -0700

    Prevent received count from being double-incremented.
    For #956.

--- spec/rspec/mocks/failure_notification_spec.rb
+++ spec/rspec/mocks/failure_notification_spec.rb
@@ -21,6 +21,15 @@ RSpec.describe "Failure notification" do
     expect(error.backtrace.first).to match(/#{File.basename(__FILE__)}:#{expected_from_line}/)
   end
 
+  it "does not allow a double to miscount the number of times a message was recevied when a failure is notified in an alternate way" do
+    dbl = double("Foo")
+    expect(dbl).not_to receive(:bar)
+
+    capture_errors { dbl.bar }
+
+    expect { verify dbl }.to fail_including("expected: 0 times", "received: 1 time")
+  end
+
   context "when using `aggregate_failures`" do
     specify 'spy failures for unreceived messages are reported correctly' do
       expect {
