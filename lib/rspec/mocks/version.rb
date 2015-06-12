f7837f522072a890b7be7ad8f71b66eca3f18de9
Myron Marston <myron.marston@gmail.com>
2015-06-12 08:35:07 -0700

    Bump version to 3.4.0.pre.

--- lib/rspec/mocks/version.rb
+++ lib/rspec/mocks/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec mocks.
     module Version
       # Version of RSpec mocks currently in use in SemVer format.
-      STRING = '3.3.0'
+      STRING = '3.4.0.pre'
     end
   end
 end
