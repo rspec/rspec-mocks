a70af7d2c62b39e0f5e6cff8953666acfb5dcc8e
Sam Phippen <samphippen@googlemail.com>
2017-05-04 19:56:40 -0400

    Releases 3.6.0

--- lib/rspec/mocks/version.rb
+++ lib/rspec/mocks/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec mocks.
     module Version
       # Version of RSpec mocks currently in use in SemVer format.
-      STRING = '3.6.0.beta2'
+      STRING = '3.6.0'
     end
   end
 end
