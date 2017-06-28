38015e1aa82bd418581db5df565ef369d43b16ad
Pat Allan <pat@freelancing-gods.com>
2017-06-28 12:32:28 +1000

    Updates for frozen-string-literal compatibility. (#1165)

--- spec/rspec/mocks/any_instance_spec.rb
+++ spec/rspec/mocks/any_instance_spec.rb
@@ -378,12 +378,12 @@ module RSpec
 
           it "works with the non-standard constructor \"\"" do
             allow_any_instance_of(String).to receive(:foo).and_return(1)
-            expect("".foo).to eq(1)
+            expect("".dup.foo).to eq(1)
           end
 
           it "works with the non-standard constructor \'\'" do
             allow_any_instance_of(String).to receive(:foo).and_return(1)
-            expect(''.foo).to eq(1)
+            expect(''.dup.foo).to eq(1)
           end
 
           it "works with the non-standard constructor module" do
@@ -545,10 +545,10 @@ module RSpec
 
         it "does not set the expectation on every instance" do
           # Setup an unrelated object of the same class that won't receive the expected message.
-          allow('non-related object').to receive(:non_related_method)
+          allow('non-related object'.dup).to receive(:non_related_method)
 
           expect_any_instance_of(Object).to receive(:foo)
-          'something'.foo
+          'something'.dup.foo
         end
 
         it "does not modify the return value of stubs set on an instance" do
