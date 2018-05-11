f08a8378f61c896b781868346c6d748a47ad9695
Andrew Walter <andrew.walter@burnet.edu.au>
2018-05-11 22:53:47 +1000

    Added `duck_type_including` ArgumentMatcher

--- lib/rspec/mocks/argument_matchers.rb
+++ lib/rspec/mocks/argument_matchers.rb
@@ -52,6 +52,15 @@ module RSpec
         DuckTypeMatcher.new(*args)
       end
 
+      # Matches if the actual argument responds to the specified messages, and the values match.
+      #
+      # @example
+      #   expect(object).to receive(:message).with(duck_type_including(name: 'Fred'))
+      #   expect(object).to receive(:message).with(duck_type_including(name: 'Fred', last_name: 'Flintstone'))
+      def duck_type_including(**args)
+        DuckTypeIncludingMatcher.new(**args)
+      end
+
       # Matches a boolean value.
       #
       # @example
@@ -268,6 +277,24 @@ module RSpec
 
         def description
           "duck_type(#{@methods_to_respond_to.map(&:inspect).join(', ')})"
+        end
+      end
+
+      # @private
+      class DuckTypeIncludingMatcher
+        def initialize(**methods_to_respond_to_with_values)
+          @methods_to_respond_to_with_values = methods_to_respond_to_with_values
+        end
+
+        def ===(value)
+          @methods_to_respond_to_with_values.all? do |message, expected_value|
+            value.respond_to?(message) && value.send(message.to_sym) == expected_value
+          end
+        end
+
+        def description
+          hash_as_string = @methods_to_respond_to_with_values.collect { |k, v| "#{k}: '#{v}'" }.join(', ')
+          "duck_type_including(#{hash_as_string})"
         end
       end
 
