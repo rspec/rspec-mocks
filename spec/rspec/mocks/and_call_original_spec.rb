ef69fe0a2cf9cc47bb3934d94c4b1b4bd5344498
Jon Rowe <hello@jonrowe.co.uk>
2021-01-15 14:48:29 +0000

    Remove kw args check

--- spec/rspec/mocks/and_call_original_spec.rb
+++ spec/rspec/mocks/and_call_original_spec.rb
@@ -185,14 +185,10 @@ RSpec.describe "and_call_original" do
       expect(klazz.alternate_new).to be_an_instance_of(klazz)
     end
 
-    if RSpec::Support::RubyFeatures.kw_args_supported?
-      binding.eval(<<-CODE, __FILE__, __LINE__)
-      it "works for methods that accept keyword arguments" do
-        def instance.foo(bar: nil); bar; end
-        expect(instance).to receive(:foo).and_call_original
-        expect(instance.foo(bar: "baz")).to eq("baz")
-      end
-      CODE
+    it "works for methods that accept keyword arguments" do
+      def instance.foo(bar: nil); bar; end
+      expect(instance).to receive(:foo).and_call_original
+      expect(instance.foo(bar: "baz")).to eq("baz")
     end
 
     context 'on an object that defines method_missing' do
