cdd9a0465a001f20479922ae3f6e45d1fa29f3a4
Yusuke Endoh <mame@ruby-lang.org>
2021-02-05 13:28:40 +0900

    Wrap the whole spec in an eval
    to avoid a warning: "assigned but unused variable - hash"

--- spec/rspec/mocks/partial_double_spec.rb
+++ spec/rspec/mocks/partial_double_spec.rb
@@ -82,14 +82,14 @@ module RSpec
       end
 
       if RSpec::Support::RubyFeatures.required_kw_args_supported?
+        # Use eval to avoid syntax error on 1.8 and 1.9
+        binding.eval(<<-CODE, __FILE__, __LINE__)
         it "can accept an inner hash as a message argument" do
           hash = {:a => {:key => "value"}}
           expect(object).to receive(:foobar).with(:key => "value").and_return(1)
-          # Use eval to avoid syntax error on 1.8 and 1.9
-          eval("expect(object.foobar(**hash[:a])).to equal(1)")
-          # Avoid warning: assigned but unused variable - hash
-          hash = hash
+          expect(object.foobar(**hash[:a])).to equal(1)
         end
+        CODE
       end
 
       it "can create a positive message expectation" do
